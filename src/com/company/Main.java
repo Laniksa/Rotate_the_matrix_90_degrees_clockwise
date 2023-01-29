02d158a45b3c4aa15f74477f163b7b53e1e0a040
Регина <rmashkovtseva@gmail.com>
2023-01-29 19:59:32 +0300

    поворот на 90,180 сделан,
    270 в работе

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,8 @@ public class Main {
     public static int num = 255;
     public static int [][] newMyMass = new int[size][size];
 
-
-
     public static void rotation180(int mas[][]){
-        for(int i = 0; i < size; i++){
+        for(int i = size-1; i >= 0; i--){
             for(int j = size-1; j >= 0; j--)  {
                 newMyMass[i][j] = mas[i][j];
                 System.out.print(newMyMass[i][j]+ " ");
@@ -31,18 +29,19 @@ public class Main {
     public static void main(String[] args) {
         Random ran = new Random();
 
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
-            myMass[i][j] = ran.nextInt(num+1);
-            System.out.print(myMass[i][j]+ " ");
-        }
-        System.out.println();
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                myMass[i][j] = ran.nextInt(num+1);
+                System.out.print(myMass[i][j]+ " ");
+            }
+            System.out.println();
 
         }
         System.out.println();
+
 
 rotation90(myMass);
-//rotation180(myMass);
+rotation180(myMass);
 
     }
 }
