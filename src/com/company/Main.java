2e21468cd7acc303776f04ee36165241cfe934be
Регина <rmashkovtseva@gmail.com>
2023-01-29 15:54:59 +0300

    Создана матрица 8 на 8 числа от 0 до 255

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int size = 8;
+    int[][] myMass = new int[size][size];
+    int num = 255;
+
+        Random ran = new Random();
+
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            myMass[i][j] = ran.nextInt(num+1);
+            System.out.print(myMass[i][j]+ " ");
+        }
+        System.out.println();
+
+        }
     }
 }
